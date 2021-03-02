3cfd60a34f4d5876273124833e56521fce024589
Franck Alex <pwd986@alumni.ku.dk>
2021-03-02 21:43:22 +0100

    adding eventBus to Game class

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -4,21 +4,34 @@ using System.IO;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
 using DIKUArcade.Math;
+using System.Collections.Generic;
+using DIKUArcade.EventBus;
 
 namespace Galaga
 {
-    public class Game
+    public class Game : IGameEventProcessor<object>
     {
         private Player player;
         private Window window;
         private GameTimer gameTimer;
+        private GameEventBus<object> eventBus;
         public Game()
         {
             window = new Window("Galaga", 500, 500);
             gameTimer = new GameTimer(30, 30);
+
             player = new Player(new DynamicShape(
                         new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
                         new Image(Path.Combine("Assets", "Images", "Player.png")));
+
+            eventBus = new GameEventBus<object>();
+            eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.
+            InputEvent });
+
+            window.RegisterEventBus(eventBus);
+            eventBus.Subscribe(GameEventType.InputEvent, this);
+
+
         }
         public void Run()
         {
@@ -35,7 +48,7 @@ namespace Galaga
                 {
                     window.Clear();
                     player.Render(); //! this works
-                    // render game entities here...
+                                     // render game entities here...
                     window.SwapBuffers();
                 }
                 if (gameTimer.ShouldReset())
@@ -43,6 +56,33 @@ namespace Galaga
                     // this update happens once every second
                     window.Title = $"Galaga | (UPS,FPS): ({gameTimer.CapturedUpdates},{gameTimer.CapturedFrames})";
                 }
+            }
+        }
+
+
+        public void KeyPress(string key)
+        {
+            // TODO: switch on key string and set the player's move direction
+        }
+
+        public void KeyRelease(string key)
+        {
+            // TODO: switch on key string and disable the player's move direction
+            // TODO: Close window if escape is pressed
+        }
+
+        public void ProcessEvent(GameEventType type, GameEvent<object> gameEvent)
+        {
+            switch (gameEvent.Parameter1)
+            {
+                case "KEY_PRESS":
+                    KeyPress(gameEvent.Message);
+                    break;
+                case "KEY_RELEASE":
+                    KeyRelease(gameEvent.Message);
+                    break;
+                default:
+                    break;
             }
         }
     }
