b68fef0a3232d9713ec1d9e67f5dc7790d517b44
tryllepus <79832662+tryllepus@users.noreply.github.com>
2021-03-02 16:55:54 +0100

    Co-authored-by: wintheriscoming <wintheriscoming@users.noreply.github.com> Co-authored-by: pwd986Vlad <pwd986Vlad@users.noreply.github.com>

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -1,16 +1,24 @@
 using DIKUArcade;
 using DIKUArcade.Timers;
+using System.IO;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
 
 namespace Galaga
 {
     public class Game
     {
+        private Player player;
         private Window window;
         private GameTimer gameTimer;
         public Game()
         {
             window = new Window("Galaga", 500, 500);
             gameTimer = new GameTimer(30, 30);
+            player = new Player(new DynamicShape(
+                        new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
+                        new Image(Path.Combine("Assets", "Images", "Player.png")));
         }
         public void Run()
         {
