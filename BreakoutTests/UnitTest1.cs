3e06e610661cc2d62a053f62faf016d9582371b9
Franck Alex <pwd986@alumni.ku.dk>
2021-04-16 19:27:36 +0200

    testUnit_Setup

---
+++ BreakoutTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using System;
+
+namespace BreakoutTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
