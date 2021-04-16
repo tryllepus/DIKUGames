70fe4c040d1f6588fa19f2637a2c4960a2179867
Franck Alex <pwd986@alumni.ku.dk>
2021-04-16 19:15:27 +0200

    breakout LetsGO!

---
+++ Breakout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Breakout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
